6e8784fcde27486e51120733c6c0ee9a7f12330c
specialyas <54225481+specialyas@users.noreply.github.com>
2021-09-14 01:15:33 +0100

    Detect when the ball misses the paddle
    The ball would bounce to the opposite direction if it misses the paddle

--- main.py
+++ main.py
@@ -36,4 +36,15 @@ while game_is_on:
     if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
         ball.bounce_x()
 
+    # Detect right paddle miss
+    if ball.xcor() > 380:
+        ball.reset_position()
+        ball.bounce_x()
+
+    # Detect left paddle miss
+    if ball.xcor() < -380:
+        ball.reset_position()
+        ball.bounce_x()
+
+
 screen.exitonclick()
